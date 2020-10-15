0b2e3fc714729b1ae0b5d20b0207fd28d3725876
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-10-16 02:38:10 +0300

    server main logged properly

--- apps/server/src/include/debug_output.hrl
+++ apps/server/src/include/debug_output.hrl
@@ -5,3 +5,7 @@
 
 -define(DEBUG_ENTRY_LOG(Args), ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args])).
 -define(DEBUG_EXIT_LOG(Args, Res), ?LOG_DEBUG("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args, Res])).
+-define(DEBUG_EXIT_LOG_N_RETURN(Args, Res), ?DEBUG_EXIT_LOG(Args, Res), Res).
+-define(HANDLE_DEBUG_LOG_N_RETURN(Args), ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]), Args).
+-define(HANDLE_ERROR_LOG_N_RETURN(Args), ?LOG_ERROR("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]), Args).
+-define(HANDLE_ERROR_LOG_N_RETURN(Args, Res), ?LOG_ERROR("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]), Args, Res).
