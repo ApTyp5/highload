19432446676e2b12cff73438e303f77c22dc5584
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-10-16 04:14:15 +0300

    error macro improved

--- apps/server/src/include/debug_output.hrl
+++ apps/server/src/include/debug_output.hrl
@@ -9,4 +9,4 @@
 -define(HANDLE_DEBUG_LOG_N_RETURN(Args), case ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Args end).
 -define(HANDLE_DEBUG_LOG_N_RETURN(Args, Res), case ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Res end).
 -define(HANDLE_ERROR_LOG_N_RETURN(Args), case ?LOG_ERROR("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Args end).
--define(HANDLE_ERROR_LOG_N_RETURN(Args, Res), case ?LOG_ERROR("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Res end).
+-define(HANDLE_ERROR_LOG_N_RETURN(Args, Res), case ?LOG_ERROR("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args, Res]) of _ -> Res end).
