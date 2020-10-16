ef110af7e88403c66b21f2151f4e2247cc212fa8
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-10-16 04:14:36 +0300

    debug macro improved

--- apps/server/src/include/debug_output.hrl
+++ apps/server/src/include/debug_output.hrl
@@ -7,6 +7,6 @@
 -define(DEBUG_EXIT_LOG(Args, Res), ?LOG_DEBUG("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args, Res])).
 -define(DEBUG_EXIT_LOG_N_RETURN(Args, Res), case ?DEBUG_EXIT_LOG(Args, Res) of _ -> Res end).
 -define(HANDLE_DEBUG_LOG_N_RETURN(Args), case ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Args end).
--define(HANDLE_DEBUG_LOG_N_RETURN(Args, Res), case ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Res end).
+-define(HANDLE_DEBUG_LOG_N_RETURN(Args, Res), case ?LOG_DEBUG("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args, Res]) of _ -> Res end).
 -define(HANDLE_ERROR_LOG_N_RETURN(Args), case ?LOG_ERROR("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Args end).
 -define(HANDLE_ERROR_LOG_N_RETURN(Args, Res), case ?LOG_ERROR("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args, Res]) of _ -> Res end).
