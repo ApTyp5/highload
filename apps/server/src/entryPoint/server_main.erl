3996ba75875c6fd71e85e2aaa4af1ae5e27a12b8
arthur_potapchuk <aptyp.potatoch@gmail.com>
2020-10-16 03:04:45 +0300

    Logging bug fixed

--- apps/server/src/entryPoint/server_main.erl
+++ apps/server/src/entryPoint/server_main.erl
@@ -1,82 +1,109 @@
 -module(server_main).
 -author("aptyp potatoch").
 
-%% behaviour
--behaviour(application).
--export([start/2, stop/1, f/0]).
-
 %% includes
 -include("../server_args.hrl").
 -include("../include/types.hrl").
+-include("../include/debug_output.hrl").
 -include("./server_props.hrl").
 -include("../supervisor/sup_props.hrl").
 -include_lib("kernel/include/logger.hrl").
 
+%% exports
+-export([stop_server/0]).
+
+%% behaviour
+-behaviour(application).
+-export([start/2, stop/1]).
+
 -spec start(Type :: atom(), ServerArgs :: #server_args{}) -> {ok, pid(), port()}.
-start(normal, Args)
-		-> ?LOG_NOTICE("starting server application with args: ~p~n", [Args]),
-%%	logger:set_module_level([server_acceptor, simple_http], debug), ?LOG_DEBUG("test"),
+start(normal, Args) ->
+	?LOG_NOTICE("starting server application with args: ~p~n", [Args]),
+	logger:set_module_level([server_main], debug), ?LOG_DEBUG("test"),
 	ServerArgs = #server_args{configPath = Args},
 	{ok, ServerProps} = args_to_props(ServerArgs),
+	?LOG_NOTICE("server props are: ~p~n", [ServerProps]),
 	{ok, ListenSock} = start_listen_socket(ServerProps),
 	{ok, SupPid} = server_sup:start_link(to_sup_props(ServerProps, ListenSock)),
 	{ok, SupPid, ListenSock}.
-stop(ListenSocket) when is_port(ListenSocket) -> ?LOG_NOTICE("stopping server application"),
+
+stop(ListenSocket) when is_port(ListenSocket) ->
+	?LOG_NOTICE("stopping server application"),
 	close_listen_socket(ListenSocket).
 
 %% internal functions
 -spec args_to_props(Args) -> classic_return(#server_props{})
 	when Args :: #server_args{}.
 
-args_to_props(Args) when is_record(Args, server_args) -> ?LOG_DEBUG("args_to_props: ~p~n", [Args]),
-	handle_read_file(
+args_to_props(Args) when is_record(Args, server_args) ->
+	?DEBUG_ENTRY_LOG(Args),
+	Res = handle_read_file(
 		file:read_file(Args#server_args.configPath)
-	).
+	),
+	?LOG_NOTICE("Res: ~p~n", [Res]),
+	Hey = ?DEBUG_EXIT_LOG_N_RETURN(Args, Res),
+	?LOG_NOTICE("Hey: ~p~n", [Hey]),
+	Hey.
 
-handle_read_file({ok, Binary}) -> ?LOG_DEBUG("handle_read_file: ok, ~p~n", [Binary]),
+
+handle_read_file({ok, Binary}) ->
+	?DEBUG_ENTRY_LOG({ok, Binary}),
 	Lines = binary:split(Binary, <<"\n">>, [global]),
 	LinesWithoutComments = clean_comments(Lines),
 	KWLists = format_lines(LinesWithoutComments),
 	TrimmedKwLists = trim_kw(KWLists),
-	{ok, generate_props(TrimmedKwLists)};
-handle_read_file({error, Reason}) -> ?LOG_DEBUG("handle_read_file: error, ~p~n", [Reason]),
-	{error, logger:error("read_file: ~p", [Reason])}.
+	?DEBUG_EXIT_LOG_N_RETURN({ok, Binary}, {ok, generate_props(TrimmedKwLists)});
+
+handle_read_file(Error) -> ?HANDLE_ERROR_LOG_N_RETURN(Error).
 
 
-generate_props(KVLists)
-		-> ?LOG_DEBUG("generate_props/1: ~p~n", [KVLists]),
-	generate_props(KVLists, #server_props{}).
+generate_props(KVLists) ->
+	?DEBUG_ENTRY_LOG(KVLists),
+	Res = generate_props(KVLists, #server_props{}),
+	?DEBUG_EXIT_LOG_N_RETURN(KVLists, Res).
 
-generate_props([], Props)
-		-> ?LOG_DEBUG("generate_props/2: ~p~n", [{[], Props}]),
-	Props;
-generate_props([KV | KVLists], Props)
-		-> ?LOG_DEBUG("generate_props/2: ~p~n", [{KV, KVLists, Props}]),
-	generate_props(KVLists, add_prop(KV, Props)).
+
+generate_props([], Props) -> ?HANDLE_DEBUG_LOG_N_RETURN([[], Props], Props);
+
+generate_props([KV | KVLists], Props) ->
+	?DEBUG_ENTRY_LOG([[KV | KVLists], Props]),
+	Res = generate_props(KVLists, add_prop(KV, Props)),
+	?DEBUG_EXIT_LOG_N_RETURN([[KV | KVLists], Props], Res).
+
 
 add_prop([<<"port">> | [Value]], Props) ->
-	#server_props{
+	?DEBUG_ENTRY_LOG([[<<"port">> | [Value]], Props]),
+	Res = #server_props{
 		workerNum = Props#server_props.workerNum,
 		root = Props#server_props.root,
 		port = binary_to_integer(Value)
-	};
+	},
+	?DEBUG_EXIT_LOG_N_RETURN([[<<"port">> | [Value]], Props], Res);
+
 add_prop([<<"worker_count">> | [Value]], Props) ->
-	#server_props{
+	?DEBUG_ENTRY_LOG([[<<"worker_count">> | [Value]], Props]),
+	Res = #server_props{
 		workerNum = binary_to_integer(Value),
 		root = Props#server_props.root,
 		port = Props#server_props.port
-	};
+	},
+	?DEBUG_EXIT_LOG_N_RETURN([[<<"worker_count">> | [Value]], Props], Res);
+
 add_prop([<<"document_root">> | [Value]], Props) ->
-	#server_props{
+	?DEBUG_ENTRY_LOG([[<<"document_root">> | [Value]], Props]),
+	Res = #server_props{
 		workerNum = Props#server_props.workerNum,
 		root = Value,
 		port = Props#server_props.port
-	};
-add_prop([_], Props) ->
-	Props.
+	},
+	?DEBUG_EXIT_LOG_N_RETURN([[<<"document_root">> | [Value]], Props], Res);
 
-clean_comments(Lines) -> ?LOG_DEBUG("clean_comments: ~p~n", [Lines]),
-	lists:map(
+add_prop([ErrorProp], Props) -> ?HANDLE_ERROR_LOG_N_RETURN([[ErrorProp], Props], Props).
+
+
+clean_comments(Lines) ->
+	?DEBUG_ENTRY_LOG(Lines),
+	Res = lists:map(
 		fun (Line) -> lists:nth(
 			1,
 			binary:split(
@@ -86,29 +113,38 @@ clean_comments(Lines) -> ?LOG_DEBUG("clean_comments: ~p~n", [Lines]),
 		)
 		end,
 		Lines
-	).
+	),
+	?DEBUG_EXIT_LOG_N_RETURN(Lines, Res).
 
-format_lines(LinesWithoutComments) -> ?LOG_DEBUG("~s: ~p~n", [?FUNCTION_NAME, LinesWithoutComments]),
-	lists:map(
+
+format_lines(LinesWithoutComments) ->
+	?DEBUG_ENTRY_LOG(LinesWithoutComments),
+	Res = lists:map(
 		fun(CleanLine) -> string:split(
 			CleanLine,
 			" "
 		)
 		end,
 		LinesWithoutComments
-	).
+	),
+	?DEBUG_EXIT_LOG_N_RETURN(LinesWithoutComments, Res).
 
-trim_kw(KWLists) -> ?LOG_DEBUG("~s/~p: ~p~n", [?FUNCTION_NAME, ?FUNCTION_ARITY, KWLists]),
-	lists:map(
+
+trim_kw(KWLists) ->
+	?DEBUG_ENTRY_LOG(KWLists),
+	Res = lists:map(
 		fun (KW) -> lists:map(
 			fun (Word) -> string:trim(Word) end,
 			KW
 		) end,
 		KWLists
-	).
+	),
+	?DEBUG_EXIT_LOG_N_RETURN(KWLists, Res).
 
-start_listen_socket(#server_props{port = Port}) -> ?LOG_DEBUG("~s/~p: ~p~n", [?FUNCTION_NAME, ?FUNCTION_ARITY, Port]),
-	gen_tcp:listen(Port, [
+
+start_listen_socket(#server_props{port = Port}) ->
+%%	?DEBUG_ENTRY_LOG(Port),
+	Options = [
 		{mode, binary},
 		{packet, 0},
 		{active, false},
@@ -117,19 +153,28 @@ start_listen_socket(#server_props{port = Port}) -> ?LOG_DEBUG("~s/~p: ~p~n", [?F
 		{sndbuf, 1024 * 1024 * 512},
 		{show_econnreset, true},
 		{backlog, 2048}
-	]).
+	],
+	?LOG_NOTICE("start listening on port ~p with options ~p~n", [Port, Options]),
+	Res = gen_tcp:listen(Port, Options),
+%%	?DEBUG_EXIT_LOG_N_RETURN(Port, Res).
+	Res.
 
 to_sup_props(#server_props{root = Root, workerNum = WorkerNum}, LSock)
-	when is_port(LSock) -> ?LOG_DEBUG("~s/~p: ~p~n", [?FUNCTION_NAME, ?FUNCTION_ARITY, {Root, WorkerNum, LSock}]),
-	#sup_start_props{
+	when is_port(LSock) ->
+	?DEBUG_ENTRY_LOG([{Root, WorkerNum}, LSock]),
+	Res = #sup_start_props{
 		root = Root,
 		acceptorNum = WorkerNum,
 		listenSock = LSock
-	}.
+	},
+	?DEBUG_EXIT_LOG_N_RETURN([{Root, WorkerNum}, LSock], Res).
+
 
 close_listen_socket(ListenSocket)
-	when is_port(ListenSocket)-> ?LOG_NOTICE("~s/~p: ~p~n", [?FUNCTION_NAME, ?FUNCTION_ARITY, ListenSocket]),
-	gen_tcp:close(ListenSocket).
+	when is_port(ListenSocket)->
+	?DEBUG_ENTRY_LOG(ListenSocket),
+	Res = gen_tcp:close(ListenSocket),
+	?DEBUG_EXIT_LOG_N_RETURN(ListenSocket, Res).
 
 
-f() -> application:stop(server), halt().
+stop_server() -> application:stop(server), halt().
--- apps/server/src/include/debug_output.hrl
+++ apps/server/src/include/debug_output.hrl
@@ -5,7 +5,8 @@
 
 -define(DEBUG_ENTRY_LOG(Args), ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args])).
 -define(DEBUG_EXIT_LOG(Args, Res), ?LOG_DEBUG("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args, Res])).
--define(DEBUG_EXIT_LOG_N_RETURN(Args, Res), ?DEBUG_EXIT_LOG(Args, Res), Res).
--define(HANDLE_DEBUG_LOG_N_RETURN(Args), ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]), Args).
--define(HANDLE_ERROR_LOG_N_RETURN(Args), ?LOG_ERROR("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]), Args).
--define(HANDLE_ERROR_LOG_N_RETURN(Args, Res), ?LOG_ERROR("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]), Args, Res).
+-define(DEBUG_EXIT_LOG_N_RETURN(Args, Res), case ?DEBUG_EXIT_LOG(Args, Res) of _ -> Res end).
+-define(HANDLE_DEBUG_LOG_N_RETURN(Args), case ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Args end).
+-define(HANDLE_DEBUG_LOG_N_RETURN(Args, Res), case ?LOG_DEBUG("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Res end).
+-define(HANDLE_ERROR_LOG_N_RETURN(Args), case ?LOG_ERROR("~p:~p(~p)~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Args end).
+-define(HANDLE_ERROR_LOG_N_RETURN(Args, Res), case ?LOG_ERROR("~p:~p(~p) -> ~p~n", [?MODULE_STRING, ?FUNCTION_NAME, Args]) of _ -> Res end).
